3d880c7fe196e8c5168aa901e3da1cb542c05823
Timur <timovichtimur39@gmail.com>
2024-07-21 11:05:46 +0200

    Настроен ROS

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/timur/Final_work/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/timur/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/timur/catkin_ws/devel;/opt/ros/noetic' != '' else []
